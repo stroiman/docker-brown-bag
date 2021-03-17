42271792c8a83ff394513c697a251cc5fc0c600f
Peter Strøiman <peter@stroiman.com>
2021-03-17 15:48:18 +0100

    Update Implement type to wrap order

--- acceptance-tests/features/PlaceOrder.feature
+++ acceptance-tests/features/PlaceOrder.feature
@@ -2,12 +2,12 @@ Feature: Register user
     Background: 
         Given the following products exist
             | SKU | Name               | Price |
-            | dfd | Docker for Dummies | $1    |
+            | dfd | Docker for Dummies | USD 1 |
 
     Scenario: Register user
         Given I am logged in as "john.doe@example.com"
         When I place an order with the following items 
             | SKU | Amount |
             | dfd | 42     |
-        Then the order has a total price of $42
+        Then the order has a total price of USD 42
         And an order confirmation mail is sent to "john.doe@example.com"
--- acceptance-tests/features/step_definitions/product-steps.js
+++ acceptance-tests/features/step_definitions/product-steps.js
@@ -1,5 +1,51 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
+const _ = require('lodash');
+
+const parseMoney = input => {
+  const matches = input.match(/^([A-Z]+) (\d+)$/);
+  return new Money({ currency: matches[1], amount: parseFloat(matches[2]) });
+}
 
 Given('the following products exist', function(dataTable) {
-  console.log(dataTable.hashes());
+  const data = dataTable
+    .hashes()
+    .map(input => _.update(input, 'Price', parseMoney))
+    .map(createProduct)
 });
+
+const isString = input => typeof(input) === "string";
+const isNumber = input => typeof(input) === "number";
+const isCurrency = input => /^[A-Z]+$/.test(input);
+const isInstanceOf = cls =>  input => input instanceof cls;
+
+/**
+ * creates a DTO class that validates the input based
+ * on the specifications passed.
+ */
+const createDto = (spec) => function(data) {
+  if (typeof(data) !== "object") throw new TypeError("data must be an object");
+  if (data === null) throw new TypeError ("data must not be null");
+
+  console.log("DATA", data);
+  for([key, validator] of _(spec).toPairs()) {
+    if (!validator(data[key])) throw new TypeError(`${key} was ${data[key]}`);
+    this[key] = data[key];
+  }
+}
+
+const Money = createDto({
+  currency: isCurrency,
+  amount: isNumber
+});
+
+const isMoney = isInstanceOf(Money);
+
+const Product = createDto({
+  sku: isString,
+  name: isString,
+  price: isMoney
+});
+
+const createProduct = (input) => {
+  return new Product(_.mapKeys(input, (_,key) => key.toLowerCase()));
+}
